d33bccb354aab8956f2fbb68cb5ea7e1741643c9
Deep Patel <118447327+deepbajud@users.noreply.github.com>
2023-08-25 14:11:19 +0530

    Commit 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -36,7 +37,15 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-analytics:21.3.0")
+    implementation("com.google.firebase:firebase-database:20.2.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    implementation("com.github.QuadFlask:colorpicker:0.0.13")
+    implementation ("com.nex3z:toggle-button-group:1.1.8")
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+
 }
